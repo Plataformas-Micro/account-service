61066dcd647236779c98842d01d9d64b93b5226b
Mateus-PortoPP <mateusppp96@gmail.com>
2026-03-11 13:48:03 -0300

    Implement Account model, service, and resource with CRUD operations

--- src/main/java/store/account/AccountModel.java
+++ src/main/java/store/account/AccountModel.java
@@ -2,6 +2,8 @@ package store.account;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -11,14 +13,38 @@ import lombok.experimental.Accessors;
 
 @Entity
 @Table(name = "accounts")
-@Setter @Accessors(chain = true,fluent = false)
+@Setter @Accessors(chain = true, fluent = true)
 @NoArgsConstructor @AllArgsConstructor
 public class AccountModel {
+
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     private String id;
 
-    @Column(name = "email",nullable = false)
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "password_sha256")
+    private String passwordSha256;
+
+    public AccountModel(Account a) {
+        this.id = a.id();
+        this.name = a.name();
+        this.email = a.email();
+        this.passwordSha256 = a.passwordSha256();
+    }
+
+    public Account to() {
+        return Account.builder()
+            .id(this.id)
+            .name(this.name)
+            .email(this.email)
+            .passwordSha256(this.passwordSha256)
+            .build();
+    }
+    
 }
--- src/main/java/store/account/AccountResource.java
+++ src/main/java/store/account/AccountResource.java
@@ -1,40 +1,62 @@
 package store.account;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class AccountResource implements AccountController {
 
-    @Override
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
-    }
+    @Autowired
+    private AccountService accountService;
 
     @Override
     public ResponseEntity<Void> create(AccountIn in) {
-        return ResponseEntity.ok().build();
+        final Account a = accountService.create(
+            AccountParser.to(in)
+        );
+        // returns a JSON in the HATEAOS standard.
+        return ResponseEntity.created(
+            ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(a.id())
+                .toUri()
+        ).build();
     }
 
     @Override
     public ResponseEntity<Void> delete(String id) {
+        accountService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
     }
 
     @Override
     public ResponseEntity<List<AccountOut>> findAll() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(
+            AccountParser.to(
+                accountService.findByAll()
+            )
+        );
     }
 
     @Override
     public ResponseEntity<AccountOut> findById(String id) {
-        AccountOut out = AccountOut.builder()
-            .name("John Doe")
-            .email("john.doe@example.com")
-            .id("123")
-            .build();
-        return ResponseEntity.ok(out);
+        Account out = accountService.findById(id);
+        return out == null ?
+            ResponseEntity.notFound().build() :
+            ResponseEntity.ok(
+                AccountParser.to(out) // transform from account to ou
+            );
     }
+
 }
